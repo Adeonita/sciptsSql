30048bfecdf371a4441459efc39420eaa9bac00a
adeonita <adeonita.sousa@gmail.com>
2020-04-19 19:51:34 -0300

    Comments

--- subquery.sql
+++ subquery.sql
@@ -1,9 +1,12 @@
 
 -- Subquery 
+
+-- Menor venda e nome do vendedor 
 select nome , marco
 from vendedores
 where marco = (select min(marco) from vendedores);
 
+-- Maior venda e nome do vendedor 
 select nome, marco
 from vendedores
 where marco = (select max(marco) from vendedores);
@@ -11,6 +14,7 @@ where marco = (select max(marco) from vendedores);
 select truncate(avg(marco), 2) as media
     from vendedores; 
 
+-- Venda acima da média e nome do vendedor 
 select nome, marco 
     from vendedores
 where marco > (select avg(marco) from vendedores);
